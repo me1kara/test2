b1f3e69d91fbda9002e7fe782a40e7637283d832
me1kara <aaddd2@naver.com>
2022-11-28 11:24:26 +0900

    오리진 브런치

--- Tire.java
+++ Tire.java
@@ -3,6 +3,6 @@ package com.lec.ex05_field;
 public class Tire {
 	private void sysou() {
 		// TODO Auto-generated method stub
-		System.out.println("asddas");
+		System.out.println("asddassfd");
 	}
 }
